a4b9c1bf3b0b040d3b8f479af36ff6b736e39848
Muhammad Luthfi <muhammadluthfi059@gmail.com>
2021-02-13 16:50:33 +0700

    hotfix bug build

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
 
-console.log(process.env.REACT_APP_API_BASEURL);
 ReactDOM.render(
   <Provider store={store}>
     <App />
